aee4f348b0c08a153ae350b92f76048ba55b9be0
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-07-05 21:44:54 +0530

    Added SplashScreen

--- android/app/src/main/java/com/lifeshare/MainActivity.java
+++ android/app/src/main/java/com/lifeshare/MainActivity.java
@@ -1,8 +1,16 @@
 package com.lifeshare;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+      }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
