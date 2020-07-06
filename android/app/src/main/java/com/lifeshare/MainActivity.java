5428e4b696ae7f099ad468b0d26fbce9b4acbe6f
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-07-06 17:44:00 +0530

    Added React Native Navigation

--- android/app/src/main/java/com/lifeshare/MainActivity.java
+++ android/app/src/main/java/com/lifeshare/MainActivity.java
@@ -1,23 +1,16 @@
 package com.lifeshare;
 
 import android.os.Bundle;
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 // react-native-splash-screen >= 0.3.1
 import org.devio.rn.splashscreen.SplashScreen; // here
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
   @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this, R.style.SplashScreenTheme);
         super.onCreate(savedInstanceState);
       }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "LifeShare";
-  }
+  
 }
